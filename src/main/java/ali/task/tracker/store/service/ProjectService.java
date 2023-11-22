baa5da124fda87e5a0df591f8706f8590ac2243a
ali <ali.akhanseriev@gmail.com>
2023-11-22 14:02:33 +0100

    some

--- src/main/java/ali/task/tracker/store/service/ProjectService.java
+++ src/main/java/ali/task/tracker/store/service/ProjectService.java
@@ -5,7 +5,7 @@ import ali.task.tracker.api.dto.ProjectDto;
 
 import java.util.List;
 import java.util.Optional;
-
+//
 public interface ProjectService {
     AckDto deleteProject(Long projectId);
 
